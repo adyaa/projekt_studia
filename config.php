07e12f8601c8580ad6ff3e8c7230ecefb04eae50
Ada Skrzypiec <adriannaskrzypiec@o2.pl>
2019-05-31 23:37:20 +0200

    change config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //sluzy do polaczenia sie z baza danych
-$con = mysqli_connect("localhost","dbusername","dbpass","dbname");
+$con = mysqli_connect("localhost","usern","password","dekarstwo");
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
